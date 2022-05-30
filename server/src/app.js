f79e612c00221b5d7d4017953bc4556e79350724
Lyall Stewart <me@lyallstewart.com>
2022-05-30 10:07:03 +0100

    Init express server and setup backend

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+module.exports = app;
